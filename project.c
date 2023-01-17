33dda2d82e578f07dc0b17973f9a22a22c9e1325
danialmd81 <danialmobinidh81@gmail.com>
2023-01-17 21:52:19 +0330

    v1.03

--- project.c
+++ project.c
@@ -10,6 +10,7 @@ typedef struct user
   int age;
   char username[30];
   char password[10]; 
+
 }user;
 
 void my_callback_on_key_arrival(char c);
@@ -21,25 +22,26 @@ void clear()
 
 void make_board();
 
-void fill_board();
-
 void menu();
 
 void sign_in();
 
 void sign_up();
 
+void game_menu_history();
+
+void game_menu();
+
 int main()
 {
-  clear();
   menu();
   
 
-  // HANDLE thread_id = start_listening(my_callback_on_key_arrival);
+  HANDLE thread_id = start_listening(my_callback_on_key_arrival);
 
 
 
-  // WaitForSingleObject(thread_id, INFINITE);
+  // WaitForSingleObject(thread_id, 1000);
   
   return 0;
 }
@@ -51,6 +53,7 @@ void my_callback_on_key_arrival(char c)
 
 void make_board(int x,int y)
 {
+  clear();
   for(int i=0 ;i<x;i++)
   {
     gotoxy(i,0);
@@ -76,15 +79,17 @@ void menu()
 {
   make_board(40,5);
   gotoxy(15,1);
-  setcolor(15);
+  setcolor(1);
   printf("1-sign in");
   gotoxy(15,2);
   printf("2-register");
   gotoxy(15,3);
-  setcolor(1);
-  printf("Enter your choice: ");
+  setcolor(2);
+  printf("Enter your choice:");
+  setcolor(15);
   int call;
-  label: scanf("%d",&call);
+  line87: scanf("%d",&call);
+
   if(call==1)
   {
     sign_in();
@@ -94,22 +99,65 @@ void menu()
     sign_up();
   }
   else
-  {
-    printf("choose wisely");
-    goto label;
+  { 
+    gotoxy(15,4);
+    setcolor(4);
+    printf("Choose Wisely.");
+    gotoxy(33,3);
+    goto line87;
   }
   
 }
 
 void sign_in()
 {
+  user check;
+  line107:make_board(40,6);
+  gotoxy(1,1);
+  setcolor(14);
+  printf("*Username:");
+  setcolor(15);
+  char username[30];
+  scanf("%s",username);
+  FILE *file1=fopen(username,"r");
+  if(file1==NULL)
+  {
+    gotoxy(1,2);
+    setcolor(4);
+    printf("Invalid Username.");
+    Sleep(2000);
+    
+    goto line107;
+  }
+  else
+  {
+    line123: gotoxy(1,2);
+    setcolor(14);
+    printf("*password:");
+    setcolor(15);
+    char password[10];
+    fread(&check,sizeof(user),1,file1);
+    scanf("%s",password);
+    if(strcmp(check.password,password)==0)
+    {
+      game_menu_history();
+    }
+    else
+    {
+      gotoxy(1,3);
+      setcolor(4);
+      printf("Invalid Password.");
+      goto line123;
+    }
+  }
   
+
+
 }
 
 void sign_up()
 {
   user new_user;
-  clear();
   make_board(40,6);
   gotoxy(1,1);
   setcolor(14);
@@ -138,5 +186,15 @@ void sign_up()
   scanf("%s",new_user.password);
   FILE *file=fopen(new_user.username,"w");
   fwrite(&new_user,sizeof(user),1,file);
+  game_menu();
 }
 
+void game_menu()
+{
+  
+}
+
+void game_menu_history()
+{
+
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,1 @@
-int call;
-  // label: scanf("%d",&call);
-  // if(call==1)
-  // {
-  //   sign_in();
-  // }
-  // else if (call==2)
-  // {
-  //   sign_up();
-  // }
-  // else
-  // {
-  //   printf("choose wisely");
-  //   goto label;
-  // }
+printf("%s",check.password);
